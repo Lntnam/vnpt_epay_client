2f21867572d9467053adcd71a749b32656623b6b
Nam Le <j251282@gmail.com>
2016-10-07 17:35:37 +0700

    test keys

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,8 +11,8 @@ abstract class TestCase extends FrameworkTestCase
     protected function createClient()
     {
         return new EpayClient('charging01', 'charging01', '00477', 'pajwtlzcb', [
-            'EPAY_PUBLIC_KEY' => __DIR__ . '/../key/Epay_Public_key.pem',
-            'PRIVATE_KEY'     => __DIR__ . '/../key/private_key.pem',
+            'EPAY_PUBLIC_KEY' => __DIR__ . '/../key/epay_public_key.test.pem.pem',
+            'PRIVATE_KEY'     => __DIR__ . '/../key/private_key.test.pem',
         ]);
     }
 }
